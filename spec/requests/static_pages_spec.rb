14c71429bde890d1e5bb21cbf339ac53811550f2
Eric VanWieren <eric@codex.org>
2012-10-18 15:02:35 -0400

    Updated the tests to look cleaner.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,55 +6,30 @@ describe "Static Pages" do
 
   describe "Home page" do
     before { visit home_path }
-    it "should have the content 'Sample App'" do
-      it { should have_selector('h1', :text => 'Sample App') }
-    end
 
-    it "should have the right title" do
-     it { should have_selector('title',
-                                :text => "#{@base_title}") }
-    end
-
-    it "should not have home in the title" do
-      it {should_not have_selector('title',
-                                :text => "#{@base_title} | Home") }
-    end
+    it { should have_selector('h1', :text => 'Sample App') }
+    it { should have_selector('title', :text => full_title('')) }
+    it { should_not have_selector('title', :text => "| Home") }
   end
 
   describe "Contact Page" do
     before { visit contact_path }
 
-    it "should have the content 'Contact'" do
-      page.should have_selector('h1', :text => 'Contact')
-    end
-    it "should have the right title" do
-      page.should have_selector('title',
-                                :text => "#{@base_title} | Contact")
-    end
+    it { should have_selector('h1', :text => 'Contact') }
+    it { should have_selector('title', :text => full_title('Contact')) }
   end
 
   describe "About Page" do
     before { visit about_path }
 
-    it "should have the content 'About Us'" do
-      page.should have_selector('h1', :text => 'About Us')
-    end
-    it "should have the right title" do
-      page.should have_selector('title',
-                                :text => "#{@base_title} | About")
-    end
+    it { should have_selector('h1', :text => 'About Us')}
+    it { should have_selector('title', :text => full_title('About'))}
   end
 
   describe "Help Page" do
     before { visit help_path }
 
-    it "should have the hl 'Help'" do
-      page.should have_selector('h1', :text => 'Help')
-    end
-
-    it "should have the right title" do
-      page.should have_selector('title',
-                                :text => "#{@base_title} | Help")
-    end
+    it { should have_selector('h1', :text => 'Help')}
+    it { should have_selector('title', :text => full_title('Help'))}
   end
 end
